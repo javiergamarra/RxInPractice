e09f55fc4c3716fdda4a868e6d2871d2af629e3d
Javier Gamarra <nhpatt@gmail.com>
2015-11-21 02:09:10 +0100

    my first observable and subscriber

---
+++ RxJavaInAction/src/com/nhpatt/rxjava/RxIsALibrary.java
@@ -0,0 +1,39 @@
+package com.nhpatt.rxjava;
+
+import org.junit.Test;
+import rx.Observable;
+import rx.Subscriber;
+
+public class RxIsALibrary {
+
+    @Test
+    public void anObservableEmitsThings() {
+        Observable<String> myObs = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hi!");
+                subscriber.onCompleted();
+            }
+        });
+
+        Subscriber<String> mySubs = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+        };
+
+        myObs.subscribe(mySubs);
+    }
+
+
+}
