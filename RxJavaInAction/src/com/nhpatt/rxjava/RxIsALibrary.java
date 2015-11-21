3fd22cf471bccb6a4f064b36c23cb7e97a1e28bb
Javier Gamarra <nhpatt@gmail.com>
2015-11-21 02:48:07 +0100

    emiting several items

--- RxJavaInAction/src/com/nhpatt/rxjava/RxIsALibrary.java
+++ RxJavaInAction/src/com/nhpatt/rxjava/RxIsALibrary.java
@@ -3,11 +3,22 @@ package com.nhpatt.rxjava;
 import org.junit.Test;
 import rx.Observable;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RxIsALibrary {
 
     @Test
     public void anObservableEmitsThings() {
         Observable.just("Hi!")
+                .subscribe(System.out::println, thr -> System.err.println(thr.getMessage()), () -> System.out.println("finished!"));
+    }
+
+    @Test
+    public void anObservableEmitsSeveralThings() {
+        List<String> severalThings = Arrays.asList("1", "2");
+
+        Observable.from(severalThings)
                 .subscribe(System.out::println, thr -> System.err.println(thr.getMessage()), () -> System.out.println("finished!"));
     }
 
