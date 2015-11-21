f2e2671fcb85ccdd45a683d703f7c467f416ea36
Javier Gamarra <nhpatt@gmail.com>
2015-11-21 02:28:58 +0100

    simplify subscriber :)

--- RxJavaInAction/src/com/nhpatt/rxjava/RxIsALibrary.java
+++ RxJavaInAction/src/com/nhpatt/rxjava/RxIsALibrary.java
@@ -3,30 +3,13 @@ package com.nhpatt.rxjava;
 import org.junit.Test;
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
 
 public class RxIsALibrary {
 
     @Test
     public void anObservableEmitsThings() {
-        Observable<String> myObs = Observable.just("Hi!");
-
-        Subscriber<String> mySubs = new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                System.out.println(s);
-            }
-        };
-
-        myObs.subscribe(mySubs);
+        Observable.just("Hi!").subscribe(System.out::println);
     }
 
 }
