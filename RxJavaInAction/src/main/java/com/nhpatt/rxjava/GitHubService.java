976a29a77ed4ec2860173c420d9f27dab6cbde9b
Javier Gamarra <nhpatt@gmail.com>
2016-02-11 13:22:44 +0100

    add headers to prevent API rate limit

--- RxJavaInAction/src/main/java/com/nhpatt/rxjava/GitHubService.java
+++ RxJavaInAction/src/main/java/com/nhpatt/rxjava/GitHubService.java
@@ -1,6 +1,7 @@
 package com.nhpatt.rxjava;
 
 import retrofit.http.GET;
+import retrofit.http.Headers;
 import retrofit.http.Path;
 import rx.Observable;
 
@@ -8,8 +9,10 @@ import java.util.List;
 
 public interface GitHubService {
     @GET("/users/{user}/repos")
+    @Headers("User-Agent: nhpatt")
     Observable<List<Repo>> listRepos(@Path("user") String user);
 
     @GET("/repos/{user}/{repo}/commits")
+    @Headers("User-Agent: nhpatt")
     Observable<List<Commit>> listCommits(@Path("user") String user, @Path("repo") String repo);
 }
