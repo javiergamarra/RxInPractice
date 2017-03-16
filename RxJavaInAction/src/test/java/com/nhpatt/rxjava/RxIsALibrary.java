abd7db9b304c9207977cf59758675e8419546b6e
Javier Gamarra <nhpatt@gmail.com>
2017-03-16 19:16:36 +0100

    solutions for talks

--- RxJavaInAction/src/test/java/com/nhpatt/rxjava/RxIsALibrary.java
+++ RxJavaInAction/src/test/java/com/nhpatt/rxjava/RxIsALibrary.java
@@ -1,6 +1,8 @@
 package com.nhpatt.rxjava;
 
 import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.observers.TestObserver;
 import io.reactivex.schedulers.Schedulers;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,48 +10,81 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
 public class RxIsALibrary {
 
-    private Retrofit retrofit;
-    private GitHubService service;
+    private TalksService service;
 
     @Before
     public void setUp() {
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com")
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://data.agenda.wedeploy.io/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
-        service = retrofit.create(GitHubService.class);
+        service = retrofit.create(TalksService.class);
     }
 
     @Test
     public void anObservableEmitsThings() {
-        Observable.just("Hi!")
-                .subscribe(
-                        System.out::println,
-                        thr -> System.err.println(thr.getMessage()),
-                        () -> System.out.println("finished!"));
+        TestObserver<String> test = Observable.just("Hola UVa!").test();
+
+        test.assertTerminated();
+        test.assertNoErrors();
+        test.assertValue("Hola UVa!");
     }
 
     @Test
     public void anObservableEmitsSeveralThings() {
         String[] severalThings = {"1", "2"};
 
-        Observable.fromArray(severalThings)
-                .subscribe(
-                        System.out::println,
-                        thr -> System.err.println(thr.getMessage()),
-                        () -> System.out.println("finished!"));
+        TestObserver<String> test = Observable.fromArray(severalThings).test();
 
+        test.assertTerminated();
+        test.assertNoErrors();
+        test.assertValueCount(2);
+    }
+
+    @Test
+    public void anObservableCanFail() {
+        TestObserver test = Observable.fromCallable(() -> {
+            throw new Exception("Fail!");
+        }).test();
+
+        test.assertTerminated();
+        test.assertErrorMessage("Fail!");
+    }
+
+    @Test
+    public void weCanRecoverFromAnError() {
+        TestObserver<String> test = Observable.fromCallable(() -> {
+            if (new Random().nextBoolean()) {
+                throw new Exception("2");
+            }
+            return "Hola!";
+        }).onErrorReturnItem("Adios")
+                .test();
+
+        test.assertTerminated();
+        test.assertNoErrors();
     }
 
     @Test
     public void aNetworkCallIsAnObservable() {
 
-        service.listRepos("nhpatt")
-                .subscribe(System.out::println);
+        TestObserver<List<Talk>> test = service.listTalks().test();
+
+        test.assertTerminated();
+        test.assertNoErrors();
+        assertThat(test.values(), is(not(empty())));
     }
+
 
     @Test
     public void mapTransformsEachElement() {
@@ -64,29 +99,26 @@ public class RxIsALibrary {
     @Test
     public void mapDoesNotWorkWellWithLists() {
 
-        service.listRepos("nhpatt")
+        service.listTalks()
                 .map(Observable::fromIterable)
-                .subscribe(System.out::println);
+                .subscribe(System.out::println, System.err::println);
     }
 
     @Test
     public void flatmapCanReturnElementsFromAnObservable() {
 
-        service.listRepos("nhpatt")
+        service.allTalks()
                 .flatMap(Observable::fromIterable)
-                .map(Repo::getName)
-                .map((s) -> s.replace("-", " "))
                 .subscribe(System.out::println);
     }
 
     @Test
     public void filteringResults() {
 
-        service.listRepos("nhpatt")
+        service.allTalks()
                 .flatMap(Observable::fromIterable)
-                .map(Repo::getName)
-                .map((s) -> s.replace("-", " "))
-                .filter((s) -> s.startsWith("Android"))
+                .map(Talk::getScore)
+                .filter(x -> x >= 3)
                 .take(2)
                 .subscribe(System.out::println);
     }
@@ -94,74 +126,77 @@ public class RxIsALibrary {
     @Test
     public void accumulatingResults() {
 
-        service.listRepos("nhpatt")
+        service.allTalks()
                 .flatMap(Observable::fromIterable)
-                .map(Repo::getName)
-                .map((s) -> s.replace("-", " "))
-                .filter((s) -> s.startsWith("Android"))
-                .take(2)
-                .map(String::length)
-                .scan((x, y) -> x * y)
+                .map(Talk::getScore)
+                .reduce((x, y) -> x + y)
                 .subscribe(System.out::println);
     }
 
     @Test
     public void accumulatingResultsTheOldWay() {
 
-        service.listRepos("nhpatt")
-                .map(
-                        (l) -> {
-                            int result = 0;
-                            int i = 0;
-                            int oldLength = 1;
-
-                            for (Repo repo : l) {
-                                String name = repo.getName();
-                                String replacedName = name.replace("-", " ");
-
-                                if (replacedName.startsWith("Android") && i < 2) {
-                                    result = replacedName.length() * oldLength;
-                                    oldLength = result;
-                                    System.out.println(result);
-                                    i++;
-                                }
-
-                            }
-                            return result;
-                        }
-                ).subscribe();
     }
 
     @Test
     public void retrievingListAndDetail() {
-
-        Observable<Repo> repo = service.listRepos("nhpatt")
+        Observable<Talk> someTalks = service.allTalks()
                 .flatMap(Observable::fromIterable)
                 .take(1);
 
-        Observable<Commit> commit = service.listCommits("nhpatt", "Android")
+        Observable<Talk> anotherTalks = service.allTalks()
                 .flatMap(Observable::fromIterable)
                 .take(1);
 
-        Observable.zip(repo, commit, this::updateCommit).subscribe(repo1 -> {
-            System.out.println(repo1.getCommit().getUrl());
-        });
+        Observable.merge(someTalks, anotherTalks).subscribe(System.out::println);
 
-    }
-
-    private Repo updateCommit(Repo o, Commit o2) {
-        o.setCommit(o2);
-        return o;
     }
 
     @Test
     public void schedulersAllowControllingTheThread() {
 
-        service.listRepos("nhpatt")
+        service.allTalks()
                 .subscribeOn(Schedulers.computation())
                 .observeOn(Schedulers.io())
                 .subscribe(System.out::println);
 
+    }
+
+    @Test
+    public void observablesAreLazy() {
+
+        List<Talk> talks = service.listTalks().blockingGet();
+
+        List<Talk> goodTalks = new ArrayList<>();
+        for (Talk talk : talks) {
+            System.out.println(talk);
+            if (isAGoodTalk(talk)) {
+                goodTalks.add(talk);
+            }
+        }
+
+        printBestTalk(goodTalks.get(0));
+
+        service.listTalks()
+                .toObservable()
+                .flatMap(Observable::fromIterable)
+                .doOnNext(System.out::println)
+                .filter(this::isAGoodTalk)
+                .firstElement()
+                .subscribe(this::printBestTalk);
+
+    }
+
+    private void printBestTalk(@NonNull Talk talk) {
+        System.out.println("Best talk: " + talk);
+    }
+
+    private boolean speakerIsAmalia(Talk talk) {
+        return talk.getSpeaker().contains("amalia");
+    }
+
+    private boolean isAGoodTalk(Talk talk) {
+        return talk.getScore() >= 3;
     }
 
 }
