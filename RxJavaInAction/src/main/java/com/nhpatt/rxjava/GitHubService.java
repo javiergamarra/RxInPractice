2fc5748f3913314b9beea5ea9530c217520db0cc
Javier Gamarra <nhpatt@gmail.com>
2017-03-16 09:58:11 +0100

    format

--- RxJavaInAction/src/main/java/com/nhpatt/rxjava/GitHubService.java
+++ RxJavaInAction/src/main/java/com/nhpatt/rxjava/GitHubService.java
@@ -8,12 +8,13 @@ import retrofit2.http.Path;
 import java.util.List;
 
 public interface GitHubService {
-	@GET("/users/{user}/repos")
-	@Headers("User-Agent: nhpatt")
-	Observable<List<Repo>> listRepos(@Path("user") String user);
 
-	@GET("/repos/{user}/{repo}/commits")
-	@Headers("User-Agent: nhpatt")
-	Observable<List<Commit>> listCommits(
-		@Path("user") String user, @Path("repo") String repo);
+    @GET("/users/{user}/repos")
+    @Headers("User-Agent: nhpatt")
+    Observable<List<Repo>> listRepos(@Path("user") String user);
+
+    @GET("/repos/{user}/{repo}/commits")
+    @Headers("User-Agent: nhpatt")
+    Observable<List<Commit>> listCommits(
+            @Path("user") String user, @Path("repo") String repo);
 }
